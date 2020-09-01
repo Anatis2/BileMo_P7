8487fb114a0872a592af8748fd41338f062d32bf
Anatis2 <claire.coubard@gmail.com>
2020-09-01 14:23:48 +0200

    mise en place de PhoneFixtures

--- BileMo/src/Entity/Phone.php
+++ BileMo/src/Entity/Phone.php
@@ -79,6 +79,11 @@ class Phone
      */
     private $createdAt;
 
+	public function __construct()
+	{
+		$this->createdAt = new \DateTime();
+	}
+
     public function getId(): ?int
     {
         return $this->id;
