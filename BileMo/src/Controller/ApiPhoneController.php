0fc19cc7fa3831dd25d5dc0884d7786fe33947f9
Anatis2 <claire.coubard@gmail.com>
2020-09-02 15:53:58 +0200

    gestion de la pagination (avant gestion des messages d'erreur)

--- BileMo/src/Controller/ApiPhoneController.php
+++ BileMo/src/Controller/ApiPhoneController.php
@@ -2,35 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Phone;
 use App\Repository\PhoneRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiPhoneController extends AbstractController
 {
-    /**
-     * @Route("/api/phones/{page<\d+>?1}", name="api_phones_index", methods={"GET"})
-     */
-    public function index(PhoneRepository $phoneRepository, Request $request)
-    {
-    	$page = $request->query->get('page');
 
-    	if(is_null($page) || $page < 1) {
-    		$page = 1;
-		}
-
-    	$limit = 10;
-
-        return $this->json($phoneRepository->findAllPhones($page, $limit), 200, []);
-    }
 
 	/**
 	 * @Route("/api/phones/{id}", name="api_phones_details", methods={"GET"})
 	 */
-    public function details(PhoneRepository $phoneRepository, $id)
+    public function details(PhoneRepository $phoneRepository, Phone $phone)
 	{
-		return $this->json($phoneRepository->find($id), 200);
+		$phone = $phoneRepository->find($phone->getId());
+
+		return $this->json($phone, 200, []);
+	}
+
+	/**
+	 * @Route("/api/phones/{page<\d+>?1}", name="api_phones_index", methods={"GET"})
+	 */
+	public function index(PhoneRepository $phoneRepository, Request $request, SerializerInterface $serializer)
+	{
+		$page = $request->query->get('page');
+		if(is_null($page) || $page < 1) {
+			$page = 1;
+		}
+		$limit = 10;
+
+		$phoneList = $phoneRepository->findAllPhones($page, $limit);
+
+		return $this->json($phoneList, 200, []);
+
 	}
 
 }
